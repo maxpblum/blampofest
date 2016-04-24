1fbd06160ab414b9c662cedab12e3793a6cf9e37
Max Blum <max@tictail.com>
2016-04-24 14:19:17 -0400

    Use more divs

--- src/index.jsx
+++ src/index.jsx
@@ -5,28 +5,35 @@ import './style.css'
 const App = React.createClass({
   render() {
     return <div className="content group">
+
         <h1><span className="names">Gigi and Max</span> are getting married!</h1>
-        <img className="silly-photo" src={require('./cute_at_wedding.jpg')} />
-        <p className="information group">
-          <label className="type-info">Saturday, October 15, 2016</label>
 
-          <label className="type-info">Ceremony</label>
-            <label className="info">
-              Union Chapel <br />
-              15 Wachusett Ave<br />
-              Hyannis Port, MA
-            </label>
+        <div className="column left">
+          <img className="silly-photo" src={require('./cute_at_wedding.jpg')} />
+        </div>
 
-          <label className="type-info">Reception</label>
-            <label className="info">
-              The Hyannis Port Club<br />
-              2 Irving Ave<br />
-              Hyannis Port, MA
-            </label>
+        <div className="column right">
+          <p className="information group">
+            <label className="type-info">Saturday, October 15, 2016</label>
 
-          <label className="type-info">Attire</label>
-            <label className="info">Semiformal</label>
-        </p>
+            <label className="type-info">Ceremony</label>
+              <label className="info">
+                Union Chapel <br />
+                15 Wachusett Ave<br />
+                Hyannis Port, MA
+              </label>
+
+            <label className="type-info">Reception</label>
+              <label className="info">
+                The Hyannis Port Club<br />
+                2 Irving Ave<br />
+                Hyannis Port, MA
+              </label>
+
+            <label className="type-info">Attire</label>
+              <label className="info">Semiformal</label>
+          </p>
+        </div>
 
       </div>
   }
